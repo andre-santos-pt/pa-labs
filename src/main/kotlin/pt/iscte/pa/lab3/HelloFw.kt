8d130a52a2177fa170c851ac4d5c74850912fa95
André L. Santos <andre.santos@iscte.pt>
2026-02-12 10:22:44 +0000

    minors

--- src/main/kotlin/pt/iscte/pa/lab3/HelloFw.kt
+++ src/main/kotlin/pt/iscte/pa/lab3/HelloFw.kt
@@ -10,23 +10,16 @@ interface Hello {
 
 class HelloFw(val confFile: String = "hello.conf") {
     fun allHellos(): List<String> =
-        (if(File(confFile).exists()) File(confFile).readLines() else emptyList())
-        .mapNotNull { className ->
-            try {
-                val clazz: KClass<*> = Class.forName(className).kotlin
-                val hello = clazz.createInstance() as Hello
-                hello.sayHello()
-            } catch (_ : Exception) {
-                System.err.println("could not instantiate $className")
-                null
+        (if (File(confFile).exists()) File(confFile).readLines() else emptyList())
+            .mapNotNull { className ->
+                try {
+                    val clazz: KClass<*> = Class.forName(className).kotlin
+                    val hello = clazz.createInstance() as Hello
+                    hello.sayHello()
+                } catch (_: Exception) {
+                    System.err.println("could not instantiate $className")
+                    null
+                }
             }
-        }
 }
 
-fun main() {
-    listOf("HelloPt","HelloIt").forEach { className ->
-        val clazz: KClass<*> = Class.forName("pt.iscte.pa.lab3.$className").kotlin
-        val hello = clazz.createInstance() as Hello
-        println(hello.sayHello() + "!")
-    }
-}
--- src/test/kotlin/pt/iscte/pa/lab4/KUnitTests.kt
+++ src/test/kotlin/pt/iscte/pa/lab4/KUnitTests.kt
@@ -24,8 +24,6 @@ class KUnitTests {
     }
 }
 
-
-
 // example class with tests
 class MyTests {
     @TestCase
