ac67cf381099474ea3b31b809a949e2fb8e5da66
André L. Santos <andre.santos@iscte.pt>
2026-04-15 12:51:46 +0100

    lab 5 minor fix

--- src/main/kotlin/pt/iscte/pa/lab5/Counter.kt
+++ src/main/kotlin/pt/iscte/pa/lab5/Counter.kt
@@ -1,9 +1,8 @@
 package pt.iscte.pa.lab5
 
 class Counter(val observer: (Int) -> Unit = {}) {
-   private var value = 0
-
-   fun value() = value
+   var value = 0
+        private set
 
    fun inc() {
        value++
