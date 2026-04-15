f69debbd2086ab862780fc88221765c0d2ee25e3
André L. Santos <andre.santos@iscte.pt>
2026-04-15 12:51:09 +0100

    Revert "lab 5 minor"
    This reverts commit 81c82a548db264e79dd48c6d0624a0c481f7f619.

--- src/main/kotlin/pt/iscte/pa/lab5/Counter.kt
+++ src/main/kotlin/pt/iscte/pa/lab5/Counter.kt
@@ -1,14 +1,17 @@
 package pt.iscte.pa.lab5
 
-class Counter() {
-   var value = 0
-        private set
+class Counter(val observer: (Int) -> Unit = {}) {
+   private var value = 0
+
+   fun value() = value
 
    fun inc() {
        value++
+       observer(value)
    }
 
    fun dec() {
        value--
+       observer(value)
    }
 }
